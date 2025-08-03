97979b9391f0588fc3753d8cf50af05187febdac
peravalivedasai <vedasai9988@gmail.com>
2025-08-03 15:07:01 +0530

    Updated project with latest changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const keep_alive=require('./keep_alive.js');
+// Render your React app
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Optional: register for PWA (offline)
 serviceWorker.unregister();
-client.login(process.env.TOKEN);
